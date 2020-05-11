00b82d900b9d200727e2b84b97fabc735e3e186a
null <notblock@hotmail.com>
2020-05-11 23:25:34 +0800

    client

---
+++ MacSocket/MacSocket/ClientWindowController.h
@@ -0,0 +1,17 @@
+//
+//  ClientWindowController.h
+//  MacSocket
+//
+//  Created by notblock on 2020/5/11.
+//  Copyright © 2020 notblock. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ClientWindowController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
